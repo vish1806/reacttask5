7b2b2dab7e4f348795a32bda95745aa2bbb24dc5
abc <abc@example.com>
2023-10-21 23:32:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AgeCalculator from './ageCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
